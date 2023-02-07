2c9155f22233489c12b78c08f9f31f41f2c35929
Renita <ira3624@icloud.com>
2023-02-07 22:21:52 +0200

    hw

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/style.css';
-import PostApiService from "./service";
+import PostApiService from './service';
 import Notiflix from 'notiflix';
 
 const form = document.querySelector('#search-form');
@@ -14,44 +14,51 @@ btn.addEventListener('click', onLoadMore);
 function onSearch(e) {
   e.preventDefault();
   const value = e.target.elements.searchQuery.value.trim();
-  if(value === ""){
-      removeClassBtn();
-      Notiflix.Notify.warning("Please enter something");
-      clearGallery();
-      return;
+  if (value === '') {
+    removeClassBtn();
+    Notiflix.Notify.warning('Please enter something');
+    clearGallery();
+    return;
   }
 
   postApiService.query = value;
   postApiService.resetPage();
   postApiService.fetchPost().then(post => {
-    if(post.hits.length === 0){
-        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-        clearGallery();
-        removeClassBtn();
-        return;
-    }else{
-     clearGallery();
-     Notiflix.Notify.success(`Hooray! We found ${post.totalHits} images`);
-     appendPostMarkup(post);
-     addClassBtn();
+    if (post.hits.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      clearGallery();
+      removeClassBtn();
+      return;
+    } else {
+      clearGallery();
+      Notiflix.Notify.success(`Hooray! We found ${post.totalHits} images`);
+      appendPostMarkup(post);
+      addClassBtn();
     }
-})
+  });
 }
 
 function onLoadMore() {
-  postApiService.fetchPost().then(post =>{
-    if(post.hits.length === 0){
-        removeClassBtn();
-        gallery.insertAdjacentHTML("beforeend", `<b>We're sorry, but you've reached the end of search results.<b>`);
-    }else{
-        appendPostMarkup(post);
+  postApiService.fetchPost().then(post => {
+    if (post.hits.length === 0) {
+      removeClassBtn();
+      gallery.insertAdjacentHTML(
+        'beforeend',
+        `<b>We're sorry, but you've reached the end of search results.<b>`
+      );
+    } else {
+      appendPostMarkup(post);
     }
   });
 }
 
 function appendPostMarkup(obj) {
- obj.hits.map(elem => {
-    gallery.insertAdjacentHTML("beforeend", `<div class="photo-card">
+  obj.hits.map(elem => {
+    gallery.insertAdjacentHTML(
+      'beforeend',
+      `<div class="photo-card">
         <img src="${elem.webformatURL}" alt="${elem.tags}" loading="lazy" />
         <div class="info">
           <p class="info-item">
@@ -71,18 +78,19 @@ function appendPostMarkup(obj) {
             ${elem.downloads}
           </p>
         </div>
-      </div>`);
+      </div>`
+    );
   });
 }
 
 function clearGallery() {
-    gallery.innerHTML = "";
+  gallery.innerHTML = '';
 }
 
 function addClassBtn() {
-    btn.classList.add("is-hidden"); 
+  btn.classList.add('is-hidden');
 }
 
 function removeClassBtn() {
-    btn.classList.remove("is-hidden");
+  btn.classList.remove('is-hidden');
 }
--- src/service.js
+++ src/service.js
@@ -1,36 +1,37 @@
-import axios from "axios";
+import axios from 'axios';
 export default class PostApiService {
-    constructor() {
-      this.searchQuery = '';
-      this.page = 1;
-    }
-  
-    async fetchPost() {
-      const url = 'https://pixabay.com/api/';
-      const key = '33448519-e193b87cc925871b53ed09026';
-        try {
-        const response = await axios.get(`${url}?key=${key}&q=${this.searchQuery}&page=${this.page}&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`);
-        this.incrementPage();
-        return response.data;
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  
-    incrementPage() {
-      this.page += 1;
-    }
-  
-    resetPage() {
-      this.page = 1;
-    }
-  
-    get query() {
-      return this.searchQuery;
-    }
-  
-    set query(newQuery) {
-      this.searchQuery = newQuery;
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchPost() {
+    const url = 'https://pixabay.com/api/';
+    const key = '33448519-e193b87cc925871b53ed09026';
+    try {
+      const response = await axios.get(
+        `${url}?key=${key}&q=${this.searchQuery}&page=${this.page}&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`
+      );
+      this.incrementPage();
+      return response.data;
+    } catch (error) {
+      console.error(error);
     }
   }
-  
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
